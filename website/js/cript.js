f27566f5651a35949c8354b4c1b298fc6eef18c2
chad74765 <133214984+chad74765@users.noreply.github.com>
2023-06-20 17:27:51 +0800

    Update cript.js

--- website/js/cript.js
+++ website/js/cript.js
@@ -1,1 +1,29 @@
 console.log("Hello, AI Information!");
+// This is a placeholder for the function that will display the news articles
+function displayNews(news) {
+    const newsContainer = document.getElementById('news');
+
+    news.forEach(article => {
+        const articleElement = document.createElement('article');
+        const titleElement = document.createElement('h2');
+        const linkElement = document.createElement('a');
+
+        titleElement.textContent = article.title;
+        linkElement.href = article.url;
+        linkElement.textContent = 'Read more';
+
+        articleElement.appendChild(titleElement);
+        articleElement.appendChild(linkElement);
+        newsContainer.appendChild(articleElement);
+    });
+}
+
+// This is a placeholder for the function that will fetch the news articles
+function fetchNews() {
+    // Fetch the news articles from the API
+    // Then call displayNews with the fetched articles
+}
+
+// Call fetchNews when the page loads
+window.onload = fetchNews;
+
