cd0e4e9787fb958b4ca980eea3fb04af53817098
chad74765 <133214984+chad74765@users.noreply.github.com>
2023-06-21 12:06:37 +0800

    Update cript.js

--- website/js/cript.js
+++ website/js/cript.js
@@ -1,29 +1,27 @@
-console.log("Hello, AI Information!");
-// This is a placeholder for the function that will display the news articles
-function displayNews(news) {
-    const newsContainer = document.getElementById('news');
-
-    news.forEach(article => {
-        const articleElement = document.createElement('article');
-        const titleElement = document.createElement('h2');
-        const linkElement = document.createElement('a');
-
-        titleElement.textContent = article.title;
-        linkElement.href = article.url;
-        linkElement.textContent = 'Read more';
-
-        articleElement.appendChild(titleElement);
-        articleElement.appendChild(linkElement);
-        newsContainer.appendChild(articleElement);
-    });
-}
-
-// This is a placeholder for the function that will fetch the news articles
-function fetchNews() {
-    // Fetch the news articles from the API
-    // Then call displayNews with the fetched articles
-}
-
-// Call fetchNews when the page loads
-window.onload = fetchNews;
+// 使用jQuery的AJAX请求从GNews API获取新闻数据
+$.ajax({
+  url: 'https://gnews.io/api/v4/search?q=AI&lang=en&token=d8261fce8b37aa3bc2d194e3818e7a24',
+  method: 'GET',
+  success: function(response) {
+    // 处理API响应，提取新闻数据
+    var articles = response.articles;
+    
+    // 在页面上显示新闻内容
+    var newsContainer = $('#news-container');
+    for (var i = 0; i < articles.length; i++) {
+      var article = articles[i];
+      var newsItem = $('<div>').addClass('news-item');
+      var title = $('<h2>').text(article.title);
+      var description = $('<p>').text(article.description);
+      var source = $('<p>').text(article.source.name);
+      
+      newsItem.append(title, description, source);
+      newsContainer.append(newsItem);
+    }
+  },
+  error: function() {
+    // 处理错误情况
+    console.log('Failed to fetch news data.');
+  }
+});
 
